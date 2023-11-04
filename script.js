4635c6abf39e97d071ac215ac95630607a02b4f2
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2023-11-05 00:55:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-const ipAdd=document.getElementsByClassName("ip")[0];
-const startBtn=document.getElementById("get-started");
+"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
 
-var location;
-
-//fetch ip address
-async function fetchIP(){
-    try{
-        const response= await fetch(`https://api.ipify.org/?format=json`)
-        const data=await response.json();
-        // console.log(data);
-        ipAdd.innerText=data.ip;
-        storeIp(data);
-    }
-    catch(error){
-        console.log(error);
-    }
-
+ async function fetchdata(){
+try{
+let res= await  fetch("https://api.ipify.org?format=json");
+let data=await res.json();
+console.log(data.ip);
+localStorage.setItem('ip',data.ip);
+ipadd.innerHTML=`Your Current IP Address is <span id="ipad">${data.ip}</span>`
+}catch(err){
+    console.log('something wrong');
 }
-
-fetchIP();
-
-
-startBtn.addEventListener("click", async()=>{
-    await navigator.geolocation.getCurrentPosition(sucess,faild)
-    // window.location.href="../main-page/main.html"
-    
+ }
+getstart.addEventListener('click',()=>{    
+    window.location.href='detail.html';
+    //gotogeo(ip);
 })
-
-
-function storeIp(data){
-    localStorage.setItem("ip", data.ip);
-
-}
-
-function sucess(position){
-    console.log(position);
-    localStorage.setItem("lat", position.coords.latitude)
-    localStorage.setItem("long", position.coords.longitude)
-    window.location.href="./main.html"
-   
-}
-
-function faild(){
-    alert("give your loacation access")
-}
+fetchdata();
